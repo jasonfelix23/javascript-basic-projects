74992199d2b6b29c3d6240a761237b61951e98d0
jasonfelix23 <jasonfelix789@gmail.com>
2022-11-15 17:07:22 -0700

    Started brushing on js

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+//#f15025
+const btn = document.getElementById('btn');
+const colors = document.querySelector('.color');
+
+btn.addEventListener('click', function (){
+    const randomVal = getColor();
+    // console.log(randomVal);
+    document.body.style.backgroundColor = randomVal;
+    colors.textContent = randomVal;
+})
+
+function getColor(){
+    let colorString = "#"
+    for(let i = 0; i < 6; i++){
+        let temp = Math.floor(Math.random()*hex.length);
+        colorString = colorString.concat(hex[temp]);
+    }
+    return colorString;
+}
